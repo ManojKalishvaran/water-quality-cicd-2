0d24546601571b536903fbe662898ff48907762c
Manoj K <CIPL1513@colanonline.local>
2025-06-30 16:48:29 +0530

    adding logging system

--- inference/main/model_loader.py
+++ inference/main/model_loader.py
@@ -1,48 +1,11 @@
-# from azure.identity import DefaultAzureCredential
-# from azure.keyvault.secrets import SecretClient
-# from azure.storage.blob import BlobServiceClient
-# import pickle
-
-# def load():
-#     keyvault_name = "manoj-key-water"
-#     secret_name = "model-blob"
-
-#     KVUri = f"https://{keyvault_name}.vault.azure.net"
-
-#     credential = DefaultAzureCredential()
-#     secret_client = SecretClient(vault_url=KVUri, credential=credential)
-
-#     sas_token = secret_client.get_secret(secret_name).value
-#     storage_account = "manojblob1"
-#     container_name = "trained-models"
-
-#     account_url = f"https://{storage_account}.blob.core.windows.net"
-#     blob_service_client = BlobServiceClient(account_url=account_url, credential=sas_token)
-
-#     container_client = blob_service_client.get_container_client(container_name)
-
-
-#     model_path = r"d:\Docker\for mlops\tarining data\manual_save\model.pkl"
-#     blob_name = "model-demo.pkl"
-#     blob_client = container_client.get_blob_client(blob_name)
-
-#     # with open(model_path, "rb") as data: 
-#     #     blob_client.upload_blob(data, overwrite=True)
-
-#     # print(f'fininshed uploading')
-
-#     blob_data = blob_client.download_blob().readall()
-
-
-#     model = pickle.loads(blob_data)
-#     return model
-
-
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from azure.storage.blob import BlobServiceClient
 from datetime import datetime
 import pickle
+import pandas as pd
+import io
+
 
 def load():
     keyvault_name = "manoj-key-water"
@@ -76,5 +39,34 @@ def load():
     model = pickle.loads(blob_data)
     return model
 
-# if __name__=="__main__":
-#     load()
+def load_blob_client():
+    feature = ["timestamp", "ph","Hardness","Solids","Chloramines","Sulfate","Conductivity","Organic_carbon","Trihalomethanes","Turbidity", "prediction"]
+    keyvault_name = "manoj-key-water"
+    secret_name = "logs"  
+
+    KVUri = f"https://{keyvault_name}.vault.azure.net"
+
+    credential = DefaultAzureCredential()
+    secret_client = SecretClient(vault_url=KVUri, credential=credential)
+
+    sas_token = secret_client.get_secret(secret_name).value
+
+    storage_account_name = "manojblob1"
+    container_name = "data-log-test"
+
+    account_url = f"https://{storage_account_name}.blob.core.windows.net"
+
+    blob_service_client = BlobServiceClient(account_url=account_url, credential=sas_token)
+
+    container_client = blob_service_client.get_container_client(container_name)
+    blob_name = "logs.csv"
+    blob_client = container_client.get_blob_client(blob_name)
+
+    if not blob_client.exists():
+
+        df = pd.DataFrame(columns= feature)
+        csv_buffer = io.StringIO()
+        df.to_csv(csv_buffer, index=False)
+        blob_client.upload_blob(csv_buffer.getvalue(), overwrite=True)
+    
+    return blob_client, feature
--- inference/main/scoring.py
+++ inference/main/scoring.py
@@ -1,10 +1,15 @@
+import io
 import pandas as pd
 from flask import Flask, request, jsonify
-from model_loader import load
+from model_loader import load, load_blob_client
+from datetime import datetime 
 
 app = Flask(__name__)
 print("loading model...")
 model = load()  
+
+print("Loading logs...")
+log_client, features = load_blob_client()
 
 print("finished loading model...")
 @app.route("/")
@@ -27,6 +32,22 @@ def score():
         # Run prediction
         predictions = model.predict(data)
 
+        log_df = pd.DataFrame(columns=features)
+
+        for i in range(len(data)):
+            values_row = list(val.item() for val in data.iloc[i].values)
+            values_row.insert(0, datetime.now())
+            values_row.append(predictions[i])
+            log_df.loc[len(log_df)] = values_row
+
+        exist_data = log_client.download_blob().readall()
+        exist_df = pd.read_csv(io.StringIO(exist_data.decode("utf-8")), sep=",")
+
+        whole_df = pd.concat([exist_df, log_df], ignore_index=True)
+        buffer_log = io.StringIO()
+        whole_df.to_csv(buffer_log, index=False)
+        log_client.upload_blob(buffer_log.getvalue(), overwrite=True)
+
         # Return predictions as JSON
         return jsonify({"predictions": predictions.tolist()})
 
@@ -35,4 +56,4 @@ def score():
     
 if __name__ == "__main__":
     # Run the Flask app on port 5001 (matching Dockerfile EXPOSE)
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=8000)
